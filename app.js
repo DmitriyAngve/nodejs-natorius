ad422f5602be2f987e4a8544a56d0a57ff1b70c4
DmitriyAngve <kirirch129@gmail.com>
2024-01-11 14:49:00 +0300

    feat: delete

--- app.js
+++ app.js
@@ -80,6 +80,36 @@ app.post('/api/v1/tours', (req, res) => {
   ); // перепишу этот файл ("..., JSON.stringify(tours)" - это данные, которые нужно взять)
 });
 
+// app.patch('/api/v1/tours/:id', (req, res) => {
+//   if (req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID',
+//     });
+//   }
+
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       tour: '<Updated tour here...>',
+//     },
+//   });
+// });
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
