d1fd7a0670e38f65ac3f86469664b82c41417894
DmitriyAngve <kirirch129@gmail.com>
2024-01-11 13:15:53 +0300

    feat: get request

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// Get from our server
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+// ---------------------------------------
+
+// (req, res) => {} - route handler
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    results: tours.length, // for clients
+    data: {
+      tours: tours, // from 21 string
+    },
+  });
 });
 
 const port = 3000;
