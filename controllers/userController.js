3b473372d029663e9ea42b74dc9bfc08f10e743d
DmitriyAngve <kirirch129@gmail.com>
2024-01-12 14:44:15 +0300

    feat: refactoring done

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+// Получить всех юзеров
+exports.getAllUsers = (req, res) => {
+  // 500 - Internal server Error
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+// Получить конкретного юзера
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+// Создать юзера
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+// Изменить юзера
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+// Удалить юзера
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
